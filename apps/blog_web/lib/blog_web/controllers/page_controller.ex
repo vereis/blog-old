5c134892da1a853090444179f69d88b65c735b24
Chris Bailey <me@cbailey.co.uk>
2022-07-06 23:26:55 +0100

    Remove unused phoenix autogen code, setup initial liveview template

--- apps/blog_web/lib/blog_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule BlogWeb.PageController do
-  use BlogWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- apps/blog_web/lib/blog_web/router.ex
+++ apps/blog_web/lib/blog_web/router.ex
@@ -1,5 +1,6 @@
 defmodule BlogWeb.Router do
   use BlogWeb, :router
+  import Phoenix.LiveDashboard.Router
 
   pipeline :browser do
     plug(:accepts, ["html"])
@@ -13,47 +14,7 @@ defmodule BlogWeb.Router do
   scope "/", BlogWeb do
     pipe_through(:browser)
 
-    get("/", PageController, :index)
+    live("/", RootLive, :home)
+    live_dashboard("/dashboard", metrics: BlogWeb.Telemetry)
   end
-
-  # Other scopes may use custom stacks.
-  #
-  # pipeline :api do
-  #   plug(:accepts, ["json"])
-  # end
-  #
-  # scope "/api", BlogWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  # coveralls-ignore-start
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through(:browser)
-      live_dashboard("/dashboard", metrics: BlogWeb.Telemetry)
-    end
-  end
-
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through(:browser)
-
-      forward("/mailbox", Plug.Swoosh.MailboxPreview)
-    end
-  end
-
-  # coveralls-ignore-stop
 end

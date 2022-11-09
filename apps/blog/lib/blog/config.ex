3a60f2f692147c2b5d1bb5cc5e1938e14bd59e83
Chris Bailey <me@cbailey.co.uk>
2022-11-09 21:26:08 +0000

    Coverage

--- apps/blog/lib/blog/config.ex
+++ apps/blog/lib/blog/config.ex
@@ -1,6 +1,7 @@
 defmodule Blog.Config do
   @moduledoc "API for fetching configuration"
 
+  # coveralls-ignore-start
   def repo!, do: System.fetch_env!("GITHUB_REPO_NAME")
   def access_token!, do: System.fetch_env!("GITHUB_REPO_ACCESS_TOKEN")
 end

d6728395fbe2d06e152faf6c389e940bfa181acb
Chris Bailey <me@cbailey.co.uk>
2022-11-09 23:08:51 +0000

    Lint

--- apps/blog/lib/blog/config.ex
+++ apps/blog/lib/blog/config.ex
@@ -2,6 +2,9 @@ defmodule Blog.Config do
   @moduledoc "API for fetching configuration"
 
   # coveralls-ignore-start
+  @spec repo! :: String.t()
   def repo!, do: System.fetch_env!("GITHUB_REPO_NAME")
+
+  @spec access_token! :: String.t()
   def access_token!, do: System.fetch_env!("GITHUB_REPO_ACCESS_TOKEN")
 end
